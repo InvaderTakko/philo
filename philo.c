7d092abe7135e3d7676b0a4a402cbf936a9c9637
Stefan Ruff <ruff1stefan@gmail.com>
2024-07-01 17:42:07 +0200

    added way to get current time/thread_locked printf/innit of most data

--- philo.c
+++ philo.c
@@ -6,55 +6,165 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 15:18:03 by sruff             #+#    #+#             */
-/*   Updated: 2024/06/29 18:27:39 by sruff            ###   ########.fr       */
+/*   Updated: 2024/07/01 17:32:22 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
+#include "philo.h"
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
 
+static int	ft_atoi(const char *str)
+{
+	int	plusminus;
+	int	number;
+	int	flag;
+
+	plusminus = 1;
+	number = 0;
+	flag = 0;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		++str;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			plusminus *= -1;
+		str++;
+	}
+	while (*str >= 48 && *str <= 57 && flag == 0)
+	{
+		number = number * 10;
+		number = number + *str - 48;
+		str++;
+		if (!(*str >= '0' && *str <= '9'))
+			flag = 1;
+	}
+	return (number * plusminus);
+}
+
+static long long	get_time(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+static void	print_status(t_data *data, int id, char *status)
+{
+	pthread_mutex_lock(&data->print_mutex);
+	if (!data->simulation_stop)
+		printf("%lld %d %s\n", get_time() - data->start_time, id + 1, status);
+	pthread_mutex_unlock(&data->print_mutex);
+}
+
+int	init_data(t_data *data, int argc, char **argv)
+{
+	int			i;
+
+	data->num_philosophers = ft_atoi(argv[1]);
+	data->philosophers = malloc(sizeof(t_philosopher) * data->num_philosophers);
+	if (!data->philosophers)
+		return (0);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->must_eat_count = ft_atoi(argv[5]);
+	data->simulation_stop = 0;
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->num_philosophers);
+	if (!data->forks)
+		return (0);
+	i = 0;
+	data->start_time = get_time();
+	while (i < data->num_philosophers)
+	{
+		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
+			return (0);
+		data->philosophers[i].id = i;
+		data->philosophers[i].left_fork = i;
+		data->philosophers[i].right_fork = i + 1;
+		data->philosophers[i].eat_count = 0;
+		data->philosophers[i].last_meal_time = data->start_time; //or get_time();
+		data->philosophers[i].data = data;
+		i++;
+	}
+	if (pthread_mutex_init(&data->print_mutex, NULL) != 0)
+		return (0);
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	t_data		data;
+	pthread_t	*threads;
+	pthread_t	monitor;
+
+	if (argc != 5 && argc != 6)
+	{
+		printf("Usage: %s number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n", argv[0]);
+		return (1);
+	}
+	if (!init_data(&data, argc, argv))
+	{
+		printf("Error initializing data\n");
+		return (1);
+	}
+	threads = malloc(sizeof(pthread_t) * data.num_philosophers);
+	if (!threads)
+		return (1);
+	while (!data.simulation_stop)
+	{
+		print_status(&data, data.philosophers->id, "is sleeping");
+		usleep(data.time_to_sleep * 1000);
+	}	
+	return (0);
+}
 // code from https://code-vault.net/lesson/18ec1942c2da46840693efe9b51eabf6
 // testing simulatanious acces with/without locking
 
-int number_go_brrr = 0;
-pthread_mutex_t mutex;
+// int number_go_brrr = 0;
+// pthread_mutex_t mutex;
 
-void	*brrr()
-{
-	int	i;
+// void	*brrr()
+// {
+// 	int	i;
 
-	i = 0;
-    while(i < 10000000)
-	{
-        pthread_mutex_lock(&mutex);
-        number_go_brrr++;
-        pthread_mutex_unlock(&mutex);
-		i++;
-    }
-	return NULL;
-}
+// 	i = 0;
+//     while(i < 10000000)
+// 	{
+//         pthread_mutex_lock(&mutex);
+//         number_go_brrr++;
+//         pthread_mutex_unlock(&mutex);
+// 		i++;
+//     }
+// 	return NULL;
+// }
 
-int main(int argc, char* argv[]) {
-    pthread_t p1, p2, p3, p4;
-    pthread_mutex_init(&mutex, NULL);
-    if (pthread_create(&p1, NULL, &brrr, NULL) != 0)
-        return 1;
-    if (pthread_create(&p2, NULL, &brrr, NULL) != 0)
-        return 2;
-    if (pthread_create(&p3, NULL, &brrr, NULL) != 0)
-        return 3;
-    if (pthread_create(&p4, NULL, &brrr, NULL) != 0)
-        return 4;
-    if (pthread_join(p1, NULL) != 0)
-        return 5;
-    if (pthread_join(p2, NULL) != 0)
-        return 6;
-    if (pthread_join(p3, NULL) != 0)
-        return 7;
-    if (pthread_join(p4, NULL) != 0)
-        return 8;
-    pthread_mutex_destroy(&mutex);
-    printf("Number go brrr %d\n", number_go_brrr);
-    return 0;
-}
+// int main(int argc, char* argv[])
+// {
+//     pthread_t p1, p2, p3, p4;
+//     pthread_mutex_init(&mutex, NULL);
+//     if (pthread_create(&p1, NULL, &brrr, NULL) != 0)
+//         return 1;
+//     if (pthread_create(&p2, NULL, &brrr, NULL) != 0)
+//         return 2;
+//     if (pthread_create(&p3, NULL, &brrr, NULL) != 0)
+//         return 3;
+//     if (pthread_create(&p4, NULL, &brrr, NULL) != 0)
+//         return 4;
+//     if (pthread_join(p1, NULL) != 0)
+//         return 5;
+//     if (pthread_join(p2, NULL) != 0)
+//         return 6;
+//     if (pthread_join(p3, NULL) != 0)
+//         return 7;
+//     if (pthread_join(p4, NULL) != 0)
+//         return 8;
+//     pthread_mutex_destroy(&mutex);
+//     printf("Number go brrr %d\n", number_go_brrr);
+//     return 0;
+// }
