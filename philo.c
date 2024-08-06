5f7182bb0755a9840cb6328620896152da5acbd6
InvaderTakko <ruff1stefan@gmail.com>
2024-08-06 17:41:13 +0200

    remove some comments

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 15:18:03 by sruff             #+#    #+#             */
-/*   Updated: 2024/07/28 18:20:05 by sruff            ###   ########.fr       */
+/*   Updated: 2024/08/06 17:40:31 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,6 +126,7 @@ static void	*monitor(void *arg)
 	// time = 0;
 	while (1)
 	{
+		//data_mutex_lock?
 		i = 0;
 		all_finished = 1;
 		while (i < data->num_philosophers)
@@ -152,6 +153,7 @@ static void	*monitor(void *arg)
 			// Allow time for the last philosopher to finish eating
 			return (NULL);
 		}
+		//data_mutex_unlock?
 		precise_usleep(250);
 	}
 	return (NULL);
@@ -306,56 +308,3 @@ int	main(int argc, char **argv)
 	// free(threads);
 	return (0);
 }
-
-// while (!data.simulation_stop)
-// {
-// 	print_status(&data, data.philosophers->id, "is sleeping");
-// 	usleep(data.time_to_sleep * 1000);
-// }
-// 	return (0);
-// }
-// code from https://code-vault.net/lesson/18ec1942c2da46840693efe9b51eabf6
-// testing simulatanious acces with/without locking
-
-// int number_go_brrr = 0;
-// pthread_mutex_t mutex;
-
-// void	*brrr(void)
-// {
-// 	int	i;
-
-// 	i = 0;
-//     while(i < 10000000)
-// 	{
-//         pthread_mutex_lock(&mutex);
-//         number_go_brrr++;
-//         pthread_mutex_unlock(&mutex);
-// 		i++;
-//     }
-// 	return (NULL);
-// }
-
-// int main(int argc, char* argv[])
-// {
-//     pthread_t p1, p2, p3, p4;
-//     pthread_mutex_init(&mutex, NULL);
-//     if (pthread_create(&p1, NULL, &brrr, NULL) != 0)
-//         return (1);
-//     if (pthread_create(&p2, NULL, &brrr, NULL) != 0)
-//         return (2);
-//     if (pthread_create(&p3, NULL, &brrr, NULL) != 0)
-//         return (3);
-//     if (pthread_create(&p4, NULL, &brrr, NULL) != 0)
-//         return (4);
-//     if (pthread_join(p1, NULL) != 0)
-//         return (5);
-//     if (pthread_join(p2, NULL) != 0)
-//         return (6);
-//     if (pthread_join(p3, NULL) != 0)
-//         return (7);
-//     if (pthread_join(p4, NULL) != 0)
-//         return (8);
-//     pthread_mutex_destroy(&mutex);
-//     printf("Number go brrr %d\n", number_go_brrr);
-//     return (0);
-// }
